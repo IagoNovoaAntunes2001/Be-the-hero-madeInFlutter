9eb789e26f989cdfd0e843ad425fe355c38f9842
Iago Novoa Antunes <inovoaan@everis.com>
2020-04-10 14:18:21 -0300

    Configuracao da arquitetura base do projeto feita, primeiras configuracoes incrementadas tambem

---
+++ lib/main.dart
@@ -0,0 +1,17 @@
+import 'package:app_be_the_hero_madeinflutter/scenes/home/home_view.dart';
+import 'package:flutter/material.dart';
+
+void main() => runApp(MyApp());
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      theme: ThemeData(
+        primarySwatch: Colors.red,
+      ),
+      home: HomeWidget(),
+    );
+  }
+}
